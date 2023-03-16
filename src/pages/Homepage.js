060911388b7e8ca6de6a2c60df59c1fa8ea82f47
Mahikagarg09 <mahika_garg@yahoo.com>
2023-03-16 21:03:46 +0530

    layout of homepage rendered

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import HeaderCards from '../components/header/header-cards/HeaderCards'
+import Overview from '../components/overview/Overview'
+import Services from '../components/services/Services'
+import Therapist from '../components/therapists/Therapist'
+import Faq from '../components/FAQ/Faq'
+import Footer from '../components/footer/Footer'
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      <HeaderCards/>
+      <Overview/>
+      <Services/>
+      <Therapist/>
+      <Faq/>
+      <Footer/>
+    </div>
   )
 }
