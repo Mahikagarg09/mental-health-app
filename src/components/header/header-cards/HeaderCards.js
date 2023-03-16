7664e4e5d254d9dc5e823b85a647e1aca1f67b82
Mahikagarg09 <mahika_garg@yahoo.com>
2023-03-16 16:53:38 +0530

    created header of homepage

--- src/components/header/header-cards/HeaderCards.js
+++ src/components/header/header-cards/HeaderCards.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import useStyles from './Headercss';
+import img from "./headerimage.png"
 
-export default function HeaderCards() {
+export default function THeaderCards() {
+  const classes = useStyles();
+
   return (
-    <div>HeaderCards</div>
-  )
+    <Box sx={{ flexGrow: 1 , backgroundColor:"#325343"}} className={classes.overall}>
+      <Grid container spacing={2}>
+        <Grid item lg={8} md={6} sm={12} xs={12}>
+          <img src={img} sx={{ maxWidth: "100%" }} alt='image' />
+        </Grid>
+        <Grid item lg={4} md={6} sm={12} xs={12}>
+          <Typography variant="h3" gutterBottom className={classes.heading}>
+            YOU DESERVE TO BE HAPPY
+          </Typography>
+          <Typography variant="h6" gutterBottom className={classes.subheading}>
+            What are you waiting for ? Go thorugh the page and find the thing which interests you!
+          </Typography>
+        </Grid>
+
+      </Grid>
+    </Box>
+  );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/header/navbar/Navbar'
+import HeaderCards from '../components/header/header-cards/HeaderCards'
 
 export default function Homepage() {
   return (
     <>
       <Navbar />
+      <HeaderCards />
     </>
   )
 }
